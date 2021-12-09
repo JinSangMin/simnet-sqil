26e56cc32430eba2958c3f992875253c1967b34e
minggii <minjikinn@gmail.com>
2021-12-09 13:53:27 +0900

    Initial commit

--- examples/simulation/split_10hr.py
+++ examples/simulation/split_10hr.py
@@ -1,3 +1,3 @@
 from l5kit.data.zarr_utils import zarr_split
 
-zarr_split('scenes/train.zarr/','scenes/', [{'name': 'train10.zarr', 'split_size_GB':0.9}, {'name': 'train90.zarr', 'split_size_GB':-1}])
+zarr_split('/data2/minji/dataset/l5kit_data/scenes/train.zarr/','/data2/minji/dataset/l5kit_data/scenes/', [{'name': 'train10.zarr', 'split_size_GB':0.9}, {'name': 'train90.zarr', 'split_size_GB':-1}])
